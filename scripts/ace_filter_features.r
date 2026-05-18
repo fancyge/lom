6fd297b23940f4ef765ce56c6200a5064b22d20d
FangWang <77766892+fancyge@users.noreply.github.com>
2026-05-18 10:35:03 +1000

    Add files via upload

---
+++ scripts/ace_filter_features.r
@@ -0,0 +1,44 @@
+##########################################################################################
+####### Preparation for ACE modelling - filter species and pathway features
+####### set cutoff for prevalence, abundance and variance
+##########################################################################################
+library(dplyr)
+library(stringr)
+library(tidyverse)
+library(compositions) 
+
+######################## filter species ########################
+sp_abun <- read.csv("./inputs/comp_metaphlan.csv")
+species_abun <- sp_abun[, -c(1:4)]
+
+prevalence_cutoff <- 0.20   # ≥20% of samples must have non-zero
+abundance_cutoff  <- 0.001  # ≥0.1% relative abundance
+var_cutoff        <- 0.001  # variance threshold
+
+n_samples <- nrow(species_abun)
+
+prev_filter <- colSums(species_abun > 0) / n_samples >= prevalence_cutoff
+abun_filter <- colMeans(species_abun) >= abundance_cutoff
+
+log_abun <- log1p(species_abun)  # log(1+x) transform
+var_filter <- apply(log_abun, 2, sd) >= var_cutoff
+
+keep_species <- prev_filter & abun_filter & var_filter
+filtered_abun <- cbind(sp_abun[, c(1:4)], species_abun[, keep_species, drop = FALSE])
+
+
+######################## filter pathways ########################
+pwy_abun <- read.delim("./inputs/func_us_425.txt")
+pwy_abun <- pwy_abun[,-1]
+epsilon <- 1e-6
+n_samples <- nrow(pwy_abun)
+
+prev_filter <- colSums(pwy_abun > epsilon) / n_samples >= 0.20
+var_cutoff  <- quantile(apply(pwy_abun, 2, sd), 0.25)   # drop lowest 25% SD
+var_filter  <- apply(pwy_abun, 2, sd) >= var_cutoff
+abun_filter <- colMeans(abs(pwy_abun)) >= 0.001
+
+keep_pwy <- prev_filter & var_filter & abun_filter
+
+filtered_pwy <- cbind(sp_abun[, c(1:4)], pwy_abun[, keep_pwy, drop = FALSE])
+
---
+++ scripts/plot_unifrac.R
@@ -0,0 +1,32 @@
+
+### ordination plot using Nonmetric Multidimensional Scaling (NMDS) based on unifrac distance 
+library(ggplot2)
+library(vegan)
+
+data <- read.delim("./inputs/unifrac_matrix.tsv")
+
+set.seed(1)
+nMDS <- metaMDS(data, distance = data, k = 2, maxit=30) ## 20 runs
+
+scores <- as.data.frame(nMDS$points) 
+meta <- read.delim("./inputs/metadata.txt", header = T, sep = "\t", row.names = 1) 
+scores.meta <- merge(scores, meta, by = 'row.names')
+
+scores.meta[,1] <- NULL
+
+colnames(scores.meta) <- c("nMDS1", "nMDS2", names(meta)) 
+
+dim(scores.meta)
+
+ggplot(scores.meta, aes(nMDS1, nMDS2, color= Timepoint)) + 
+  geom_point(size = 0.5, alpha = 1)  + 
+  theme(axis.text = element_blank()) +ggtitle("Weighted UniFrac") + theme_bw() + stat_ellipse(show.legend = FALSE,linewidth = 0.2)+scale_colour_manual(values=c("T1"="#424242", "T2"="#E64626", "T3"="#0148A4"))+labs(color="Timepoints")+ theme(
+    axis.text = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+       axis.line = element_blank(),  # this line can be omitted or removed since you specify x and y separately
+    text = element_text(size = 10),
+    legend.position = ""
+  )
+
